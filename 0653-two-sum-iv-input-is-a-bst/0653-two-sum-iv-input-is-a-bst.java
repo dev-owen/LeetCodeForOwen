39058277a34e3fc3a92012babd129799ed273584
Wonjong Oh (owen.oh) <39744246+dev-owen@users.noreply.github.com>
2022-10-18 21:18:25 +0900

    Time: 7 ms (52.44%), Space: 52.1 MB (25.15%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+        HashSet<Integer> hs = new HashSet<>();
+        return findTargetHelper(root, k, hs);
+    }
+    
+    public boolean findTargetHelper(TreeNode root, int k, Set<Integer> set) {
+        if(root == null) return false;
+        if(set.contains(root.val)) return true;
+        set.add(k - root.val);
+        return findTargetHelper(root.left, k, set) || findTargetHelper(root.right, k, set);
+    }
+}
